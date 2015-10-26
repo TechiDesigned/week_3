9d182d6aafaf93d0666b32d73c0678f04c842da1
Ski Blanchard <ski.blanchard@careerbuilder.com>
2015-10-26 18:42:16 -0400

    Week 3 hw

---
+++ number_guess_exercise.rb
@@ -0,0 +1,14 @@
+number = 10
+dog_sound = "Bark!"
+cat_sound = "Meow!"
+
+print "What's your number (1 to 100)?"
+guess_num = gets.chomp
+
+if guess_num == number
+	puts "Wow! You've guessed correctly."
+elsif guess_num.to_i >= 5 and guess_num.to_i <= 15
+	puts "Oh! So close."
+else 
+	puts "Wrong!"
+end
