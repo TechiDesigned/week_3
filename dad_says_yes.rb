67377d4963cc420bccb5c1cae243ae38e42a1a23
Ski Blanchard <ski.blanchard@careerbuilder.com>
2015-11-05 14:46:13 -0500

    print to 0 and until dad says yes exercises completed

---
+++ dad_says_yes.rb
@@ -0,0 +1,9 @@
+dad = ""
+
+until dad == "yes"
+
+print "Can we go to Itchy and Sratchy land? "
+
+dad = gets.chomp
+
+end
---
+++ print_to_0.rb
@@ -0,0 +1,11 @@
+print "Please enter a number between 1 and 10: "
+
+num = gets.chomp.to_i
+
+until num < 0
+
+	puts num
+
+	num -= 1
+
+end
